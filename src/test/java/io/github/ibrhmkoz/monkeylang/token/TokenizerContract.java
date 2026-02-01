107d9745e8d5d5cdc89073d66579f4ab7bc84705
ibrahimkoz <ibrahim.koz@edgedelta.com>
2026-02-01 20:42:39 +0300

    Add SimpleProgram test

--- src/test/java/io/github/ibrhmkoz/monkeylang/token/TokenizerContract.java
+++ src/test/java/io/github/ibrhmkoz/monkeylang/token/TokenizerContract.java
@@ -20,4 +20,64 @@ public interface TokenizerContract {
         Tokenizer tokenizer = createTokenizer("");
         assertEquals(Token.Eof.INSTANCE, tokenizer.nextToken());
     }
+
+    @Test
+    default void testNextToken_SimpleProgram() {
+        var input = """
+                let five = 5;
+                let ten = 10;
+                let add = fn(x, y) {
+                    x + y;
+                };
+                let result = add(five, ten);
+                """;
+
+        var expected = new Token[]{
+                Token.Let.INSTANCE,
+                new Token.Ident("five"),
+                Token.Assign.INSTANCE,
+                new Token.Int(5),
+                Token.Semicolon.INSTANCE,
+
+                Token.Let.INSTANCE,
+                new Token.Ident("ten"),
+                Token.Assign.INSTANCE,
+                new Token.Int(10),
+                Token.Semicolon.INSTANCE,
+
+                Token.Let.INSTANCE,
+                new Token.Ident("add"),
+                Token.Assign.INSTANCE,
+                Token.Function.INSTANCE,
+                Token.LParen.INSTANCE,
+                new Token.Ident("x"),
+                Token.Comma.INSTANCE,
+                new Token.Ident("y"),
+                Token.RParen.INSTANCE,
+                Token.LBrace.INSTANCE,
+                new Token.Ident("x"),
+                Token.Plus.INSTANCE,
+                new Token.Ident("y"),
+                Token.Semicolon.INSTANCE,
+                Token.RBrace.INSTANCE,
+                Token.Semicolon.INSTANCE,
+
+                Token.Let.INSTANCE,
+                new Token.Ident("result"),
+                Token.Assign.INSTANCE,
+                new Token.Ident("add"),
+                Token.LParen.INSTANCE,
+                new Token.Ident("five"),
+                Token.Comma.INSTANCE,
+                new Token.Ident("ten"),
+                Token.RParen.INSTANCE,
+                Token.Semicolon.INSTANCE,
+        };
+
+        Tokenizer tokenizer = createTokenizer(input);
+        for (Token token : expected) {
+            assertEquals(token, tokenizer.nextToken());
+        }
+        assertEquals(Token.Eof.INSTANCE, tokenizer.nextToken());
+    }
 }
